576dc68668f89a36687bef4b5901412f712e7169
CMAXMAPEN <anthonys4.calderon@gmail.com>
2025-03-15 22:57:49 -0400

    Added guns to map, worked on navmesh a bit
    Need to work on navmesh more and need to add colliders to props

--- shooterTesting/Assets/_Project/Scripts/buyBarriers.cs
+++ shooterTesting/Assets/_Project/Scripts/buyBarriers.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using Platformer397;
+using UnityEngine.AI;
+using Unity.AI.Navigation;
 public class buyBarriers : MonoBehaviour
 {
     [SerializeField] private int price; //to change in editor as different doors might cost different amounts
+
     public void purchased()
     {
         Debug.Log("You bought the door");
